68233a8863c0454459cc12d0d042fd9369466a55
amogu <d.krolikov@gmail.com>
2026-08-14 17:54:45 +0300

    Initial commit: baseline for automated code review tracking

---
+++ src/main/java/me/dkprog/castle_defence/Castle_defence.java
@@ -0,0 +1,16 @@
+package me.dkprog.castle_defence;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Castle_defence extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Castle_defence enabled successfully!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Castle_defence disabled.");
+    }
+}

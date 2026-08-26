dcee82ecab0808b05289ffcefddb8d4bf493a5ab
amogu <d.krolikov@gmail.com>
2026-08-26 12:28:38 +0300

    Debug commands can clear text displays now

--- src/main/java/me/dkprog/castle_defence/building/HpVisualizeCommand.java
+++ src/main/java/me/dkprog/castle_defence/building/HpVisualizeCommand.java
@@ -1,24 +1,36 @@
 package me.dkprog.castle_defence.building;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.TextDisplay;
+
+import java.util.List;
 
 public class HpVisualizeCommand implements CommandExecutor {
-    private final BuildSlot slot;
+    private final List<BuildSlot> buildSlots;
     private final BuildScanner scanner;
     private final WallDamageTracker damageTracker;
 
-    public HpVisualizeCommand(BuildSlot slot, BuildScanner scanner, WallDamageTracker damageTracker) {
-        this.slot = slot;
+    public HpVisualizeCommand(List<BuildSlot> buildSlots, BuildScanner scanner, WallDamageTracker damageTracker) {
+        this.buildSlots = buildSlots;
         this.scanner = scanner;
         this.damageTracker = damageTracker;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        scanner.visualizeBlockHp(slot, damageTracker);
-        sender.sendMessage("HP visualized");
+        for (World world : Bukkit.getWorlds()) {
+            for (TextDisplay entity : world.getEntitiesByClass(TextDisplay.class)) {
+                entity.remove();
+            }
+        }
+        for (BuildSlot slot : buildSlots) {
+            scanner.visualizeBlockHp(slot, damageTracker);
+            sender.sendMessage("HP visualized");
+        }
         return true;
     }
 }
--- src/main/java/me/dkprog/castle_defence/building/NeighborsVisualizeCommand.java
+++ src/main/java/me/dkprog/castle_defence/building/NeighborsVisualizeCommand.java
@@ -1,22 +1,34 @@
 package me.dkprog.castle_defence.building;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.TextDisplay;
+
+import java.util.List;
 
 public class NeighborsVisualizeCommand implements CommandExecutor {
-    private final BuildSlot slot;
+    private final List<BuildSlot> buildSlots;
     private final BuildScanner scanner;
 
-    public NeighborsVisualizeCommand(BuildSlot slot, BuildScanner scanner) {
-        this.slot = slot;
+    public NeighborsVisualizeCommand(List<BuildSlot> buildSlots, BuildScanner scanner) {
+        this.buildSlots = buildSlots;
         this.scanner = scanner;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        scanner.visualizeNeighborCount(slot);
-        sender.sendMessage("Visualized");
+        for (World world : Bukkit.getWorlds()) {
+            for (TextDisplay entity : world.getEntitiesByClass(TextDisplay.class)) {
+                entity.remove();
+            }
+        }
+        for (BuildSlot slot : buildSlots) {
+            scanner.visualizeNeighborCount(slot);
+            sender.sendMessage("Visualized");
+        }
         return true;
     }
 
